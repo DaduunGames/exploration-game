317b78b6f207957d0810789026081b094964ba69
Lachlan <mindcraftlachlan@outlook.com>
2022-04-03 23:51:10 +1000

    Auto stash before revert of "Merge remote-tracking branch 'refs/remotes/origin/main'"

---
+++ Assets/BuildStation.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BuildStation : TriggerCore
+{
+    public StructureBase[] Structures;
+    public int currentStructure = 0;
+
+    Hotbar hotbar;
+    public TextMeshPro Display;
+
+    private void Start()
+    {
+        hotbar = FindObjectOfType<Hotbar>();
+        SetText();
+    }
+
+    public override void Trigger()
+    {
+        if (currentStructure == Structures.Length - 1) return;
+        bool success = true;
+        foreach(ItemAndAmount ia in Structures[currentStructure+1].Cost)
+        {
+            if (!hotbar.Checkfor(ia))
+            {
+                success = false;
+            }
+            else
+            {
+                hotbar.RemoveFromInventory(ia);
+            }
+        }
+
+        if (success)
+        {
+            UpgradeStructure();
+        }
+
+    }
+
+    void UpgradeStructure()
+    {
+        foreach(StructureBase sb in Structures)
+        {
+            sb.obj.SetActive(false);
+        }
+        
+        ++currentStructure;
+        Structures[currentStructure].obj.SetActive(true);
+
+        SetText();
+    }
+
+    void SetText()
+    {
+        string txt = "";
+        foreach(ItemAndAmount ia in Structures[currentStructure+1].Cost)
+        {
+            txt += $"\n {ia.amount} x {ItemDB.ItemLibrary[(int)ia.item].Name}";
+        }
+        Display.text = txt;
+    }
+}
